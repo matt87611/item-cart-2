55a8c94815e36d46b2eff51617fccd9a5d8e7fc2
Matt Long <matt87611@gmail.com>
2016-07-31 18:48:29 +0100

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+2.times { Item.create!(name: "Item", description: "This is a description!") }
