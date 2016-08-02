64f9bae3974ec4a4f1955b6f22149ec1a1ae8871
Matt Long <matt87611@gmail.com>
2016-08-02 21:38:49 +0100

    Added ajax get request

--- app/assets/javascripts/components/body.js.jsx
+++ app/assets/javascripts/components/body.js.jsx
@@ -1,9 +1,21 @@
 class Body extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    $.getJSON('/items/show', (response) => { this.setState({ items: response }) });
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello word</h1>
+        <h1>All Items</h1>
       </div>
     );
-  };
+  }
 }; 
