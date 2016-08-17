13e6f9f3409de68399f013331d8ee9ec09ca2e37
matt87611 <matt87611@gmail.com>
2016-08-17 20:20:24 +0100

    Added create for controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,19 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def create
+  	respond_with Item.create(item_params)
+  end
+
+  def update
+  	item = Item.find(params["id"])
+  	item.update_attributes(item_params)
+  	respond_with item, json: item
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:id, :name, :description)
+  end
 end
