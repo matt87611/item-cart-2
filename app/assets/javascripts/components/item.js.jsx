52f490a68cc5340fcb416fa077dc17527c393ab1
Matt Long <matt87611@gmail.com>
2016-09-03 20:12:06 +0100

    Added state

--- app/assets/javascripts/components/item.js.jsx
+++ app/assets/javascripts/components/item.js.jsx
@@ -1,13 +1,15 @@
 var Item = React.createClass ({
   getInitialState: function() {
-	return {item: this.props.item}
+	return {
+      item: this.props.item
+    }
   },
 
   render: function() {
   	return (
       <tr>
-        <td>item.name</td>
-        <td>item.description</td>
+        <td>{this.state.item.name}</td>
+        <td>{this.state.item.description}</td>
       </tr>
   	);
   }
